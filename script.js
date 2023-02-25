720253e2440b87f485eb2f684b8d9195deee6c03
wsteo <35697328+wsteo@users.noreply.github.com>
2023-02-26 01:07:42 +0800

    Add Etch a Sketch functionality

--- script.js
+++ script.js
@@ -1,14 +1,42 @@
 const container = document.querySelector('#container');
 
-for (let i = 0; i < 16; i++)
-{
-    const row = document.createElement('div');
-    row.classList.add('row');
-    container.appendChild(row);
-    for (let j = 0; j < 16; j++)
-    {
-        const square = document.createElement('div');
-        square.classList.add('square');
-        row.appendChild(square);
+const setSizeBtn = document.querySelector("#setSizeBtn");
+setSizeBtn.addEventListener('click', generateSquare);
+
+function generateSquare(e) {
+    clearGrid();
+    console.log(e);
+    let sizeNumber = prompt("Enter a size less than 100", 16);
+    generateGrid(sizeNumber);
+    activeHoveringEffect();
+}
+
+function generateGrid(size) {
+    for (let i = 0; i < size; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        container.appendChild(row);
+        for (let j = 0; j < size; j++) {
+            const square = document.createElement('div');
+            square.classList.add('square');
+            row.appendChild(square);
+        }
     }
+}
+
+function clearGrid() {
+    const squares = document.querySelectorAll('div.square');
+    squares.forEach((square) => {
+        square.remove();
+    });
+}
+
+function activeHoveringEffect() {
+    const squares = document.querySelectorAll('div.square');
+    console.log(squares);
+    squares.forEach((square) => {
+        square.addEventListener('mouseover', function (e) {
+            this.classList.add('hovering');
+        });
+    });
 }
